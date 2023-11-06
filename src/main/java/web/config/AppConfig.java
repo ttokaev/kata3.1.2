799ee31524c22b18cef12f36a613d415c449fd72
ttokaev <toktim1709@bk.ru>
2023-11-06 12:50:07 +0300

    spring boot change

--- src/main/java/web/config/AppConfig.java
+++ src/main/java/web/config/AppConfig.java
@@ -1,73 +1,19 @@
 package web.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
-import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
-import javax.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
-import java.util.Properties;
-
+import java.util.List;
 
 @Configuration
-@PropertySource("classpath:db.properties")
-@EnableTransactionManagement
 public class AppConfig {
 
-   private final Environment env;
-
-   @Autowired
-   public AppConfig(Environment env) {
-      this.env = env;
-   }
-
-   @Bean
-   public DataSource getDataSource() {
-      DriverManagerDataSource dataSource = new DriverManagerDataSource();
-      dataSource.setDriverClassName(env.getProperty("db.driver"));
-      dataSource.setUrl(env.getProperty("db.url"));
-      dataSource.setUsername(env.getProperty("db.username"));
-      dataSource.setPassword(env.getProperty("db.password"));
-      return dataSource;
-   }
-
-   @Bean
-   public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-      LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
-
-      entityManagerFactoryBean.setDataSource(getDataSource());
-      entityManagerFactoryBean.setPackagesToScan("web.model");
-      entityManagerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
-      entityManagerFactoryBean.setJpaProperties(additionalProperties());
-
-      return entityManagerFactoryBean;
-   }
-
-   @Bean
-   public PlatformTransactionManager getTransactionManager(EntityManagerFactory emf) {
-      JpaTransactionManager transactionManager = new JpaTransactionManager();
-      transactionManager.setEntityManagerFactory(emf);
-      return transactionManager;
-   }
-
-   @Bean
-   public PersistenceExceptionTranslationPostProcessor getExceptionTranslation(){
-      return new PersistenceExceptionTranslationPostProcessor();
-   }
-
-   Properties additionalProperties() {
-      Properties properties = new Properties();
-      properties.setProperty("hibernate.hbm2ddl.auto", "update");
-      properties.setProperty("hibernate.show_sql", "true");
-      return properties;
-   }
+    @Bean
+    public FilterRegistrationBean<?> hiddenHttpMethodFilter() {
+        FilterRegistrationBean<?> filterRegistrationBean = new FilterRegistrationBean<>(new HiddenHttpMethodFilter());
+        filterRegistrationBean.setUrlPatterns(List.of("/*"));
+        return filterRegistrationBean;
+    }
 }
